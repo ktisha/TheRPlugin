c9513bf349ee3b8af17f5abe30b5410e443879d9
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-04-27 17:14:56 +0400

    added tests for semicolon parsing

--- testSrc/com/jetbrains/ther/parser/TheRParsingTest.java
+++ testSrc/com/jetbrains/ther/parser/TheRParsingTest.java
@@ -93,6 +93,10 @@ public class TheRParsingTest extends ParsingTestCase {
     doTest();
   }
 
+  public void testSemicolon() {
+    doTest();
+  }
+
   public void doTest() {
     doTest(true);
   }
