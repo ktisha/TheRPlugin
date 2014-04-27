12e2216998abbbf734c9f5bde99a4688645d37bf
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-04-27 17:35:28 +0400

    added brace matcher

---
+++ src/com/jetbrains/ther/highlighting/TheRBraceMatcher.java
@@ -0,0 +1,32 @@
+package com.jetbrains.ther.highlighting;
+
+import com.intellij.lang.BracePair;
+import com.intellij.lang.PairedBraceMatcher;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.tree.IElementType;
+import com.jetbrains.ther.lexer.TheRTokenTypes;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class TheRBraceMatcher implements PairedBraceMatcher {
+  private final BracePair[] PAIRS = new BracePair[]{
+    new BracePair(TheRTokenTypes.LPAR, TheRTokenTypes.RPAR, false),
+      new BracePair(TheRTokenTypes.LBRACKET, TheRTokenTypes.RBRACKET, false),
+      new BracePair(TheRTokenTypes.LDBRACKET, TheRTokenTypes.RDBRACKET, false),
+      new BracePair(TheRTokenTypes.LBRACE, TheRTokenTypes.RBRACE, false)};
+
+  @Override
+  public BracePair[] getPairs() {
+    return PAIRS;
+  }
+
+  @Override
+  public boolean isPairedBracesAllowedBeforeType(@NotNull IElementType lbraceType, @Nullable IElementType contextType) {
+    return false;
+  }
+
+  @Override
+  public int getCodeConstructStart(final PsiFile file, int openingBraceOffset) {
+    return openingBraceOffset;
+  }
+}
