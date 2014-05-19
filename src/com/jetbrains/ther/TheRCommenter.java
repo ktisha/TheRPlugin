aecc18fbba232f618fde2d94670f72046afe1949
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-05-19 16:52:35 +0400

    added commenter

---
+++ src/com/jetbrains/ther/TheRCommenter.java
@@ -0,0 +1,76 @@
+package com.jetbrains.ther;
+
+import com.intellij.codeInsight.generation.IndentedCommenter;
+import com.intellij.lang.CodeDocumentationAwareCommenter;
+import com.intellij.psi.PsiComment;
+import com.intellij.psi.tree.IElementType;
+import com.jetbrains.ther.lexer.TheRTokenTypes;
+import org.jetbrains.annotations.Nullable;
+
+public class TheRCommenter implements CodeDocumentationAwareCommenter, IndentedCommenter {
+  @Override
+  public String getLineCommentPrefix() {
+    return "# ";
+  }
+
+  @Override
+  public String getBlockCommentPrefix() {
+    return null;
+  }
+
+  @Override
+  public String getBlockCommentSuffix() {
+    return null;
+  }
+
+  @Override
+  public String getCommentedBlockCommentPrefix() {
+    return null;
+  }
+
+  @Override
+  public String getCommentedBlockCommentSuffix() {
+    return null;
+  }
+
+  @Override
+  public IElementType getLineCommentTokenType() {
+    return TheRTokenTypes.END_OF_LINE_COMMENT;
+  }
+
+  @Override
+  public IElementType getBlockCommentTokenType() {
+    return null;
+  }
+
+  @Override
+  public IElementType getDocumentationCommentTokenType() {
+    return null;
+  }
+
+  @Override
+  public String getDocumentationCommentPrefix() {
+    return null;
+  }
+
+  @Override
+  public String getDocumentationCommentLinePrefix() {
+    return null;
+  }
+
+  @Override
+  public String getDocumentationCommentSuffix() {
+    return null;
+  }
+
+  @Override
+  public boolean isDocumentationComment(PsiComment element) {
+    return false;
+  }
+
+  @Nullable
+  @Override
+  public Boolean forceIndentedLineComment() {
+    return true;
+  }
+}
