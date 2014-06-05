bdfbc5af794d6b263d4243b53ed6d517221e7ff8
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-06-05 15:43:44 +0400

    fixed hang on completion after namespace access

--- src/com/jetbrains/ther/parsing/TheRExpressionParsing.java
+++ src/com/jetbrains/ther/parsing/TheRExpressionParsing.java
@@ -447,7 +447,7 @@ public class TheRExpressionParsing extends Parsing {
       }
       else if (TheRTokenTypes.NAMESPACE_ACCESS.contains(tokenType)) {
         myBuilder.advanceLexer();
-        parsePrimaryExpression();
+        checkMatches(TheRTokenTypes.IDENTIFIER, "Expected name");
         expr.done(TheRElementTypes.REFERENCE_EXPRESSION);
         expr = expr.precede();
       }
