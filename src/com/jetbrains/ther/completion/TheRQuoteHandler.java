d045ff31ca46ccddf267ae2c100dc025380cf3ed
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-06-12 22:40:06 +0400

    improved quote completion

--- src/com/jetbrains/ther/completion/TheRQuoteHandler.java
+++ src/com/jetbrains/ther/completion/TheRQuoteHandler.java
@@ -1,10 +1,34 @@
 package com.jetbrains.ther.completion;
 
 import com.intellij.codeInsight.editorActions.SimpleTokenSetQuoteHandler;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.highlighter.HighlighterIterator;
 import com.jetbrains.ther.lexer.TheRTokenTypes;
 
 public class TheRQuoteHandler extends SimpleTokenSetQuoteHandler {
   public TheRQuoteHandler() {
     super(TheRTokenTypes.STRING_LITERAL);
   }
+
+  @Override
+  public boolean hasNonClosedLiteral(Editor editor, HighlighterIterator iterator, int offset) {
+    final Document document = editor.getDocument();
+    int lineEndOffset = document.getLineEndOffset(document.getLineNumber(offset));
+    if (offset < lineEndOffset) {
+      final CharSequence charSequence = document.getCharsSequence();
+      final char openQuote = charSequence.charAt(offset);
+      final int nextCharOffset = offset + 1;
+      if (nextCharOffset < lineEndOffset && charSequence.charAt(nextCharOffset) == openQuote) {
+        return true;
+      }
+      for (int i = nextCharOffset + 1; i < lineEndOffset; i++) {
+        if (charSequence.charAt(i) == openQuote) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
 }
