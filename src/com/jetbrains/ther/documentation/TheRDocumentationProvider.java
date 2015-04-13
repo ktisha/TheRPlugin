6e5c402282ea9b6f898c657bd20acfe96bddef7e
avesloguzova <sashickk@gmail.com>
2015-04-13 22:33:42 +0300

    Add Documentation provider to project (cherry picked from commit ecd4dc9)

---
+++ src/com/jetbrains/ther/documentation/TheRDocumentationProvider.java
@@ -0,0 +1,15 @@
+package com.jetbrains.ther.documentation;
+
+import com.intellij.lang.documentation.AbstractDocumentationProvider;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.Nullable;
+
+
+public class TheRDocumentationProvider extends AbstractDocumentationProvider {
+
+  @Nullable
+  @Override
+  public String generateDoc(PsiElement element, @Nullable PsiElement element1) {
+    return null;
+  }
+}
