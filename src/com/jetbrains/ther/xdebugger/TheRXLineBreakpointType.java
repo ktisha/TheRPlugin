4e1e33d8e977c0b8fa72011c36c96082cb059659
Semyon Proshev <s-proshev@ya.ru>
2015-07-15 21:27:58 +0300

    Debugger: Fix #17. Update breakpoint#canPutAt method to use Psi structure, it should be faster than previous version. Add '{' and '}' as forbidden lines.

--- src/com/jetbrains/ther/xdebugger/TheRXLineBreakpointType.java
+++ src/com/jetbrains/ther/xdebugger/TheRXLineBreakpointType.java
@@ -2,17 +2,17 @@ package com.jetbrains.ther.xdebugger;
 
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiDocumentManager;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
+import com.intellij.psi.*;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.util.Processor;
+import com.intellij.xdebugger.XDebuggerUtil;
 import com.intellij.xdebugger.breakpoints.XBreakpointProperties;
 import com.intellij.xdebugger.breakpoints.XLineBreakpoint;
 import com.intellij.xdebugger.breakpoints.XLineBreakpointType;
 import com.intellij.xdebugger.evaluation.XDebuggerEditorsProvider;
 import com.jetbrains.ther.TheRFileType;
-import com.jetbrains.ther.debugger.utils.TheRDebuggerUtils;
+import com.jetbrains.ther.parsing.TheRElementTypes;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -40,7 +40,7 @@ public class TheRXLineBreakpointType extends XLineBreakpointType<XBreakpointProp
 
   @Override
   public boolean canPutAt(@NotNull final VirtualFile file, final int line, @NotNull final Project project) {
-    return isTheRFile(file) && !isCommentOrSpaces(file, line, project);
+    return isTheRFile(file) && isStoppable(file, line, project);
   }
 
   @Nullable
@@ -50,24 +50,41 @@ public class TheRXLineBreakpointType extends XLineBreakpointType<XBreakpointProp
     return myProvider;
   }
 
-  private boolean isTheRFile(final @NotNull VirtualFile file) {
+  private boolean isTheRFile(@NotNull final VirtualFile file) {
     final String defaultExtension = TheRFileType.INSTANCE.getDefaultExtension();
     final String extension = file.getExtension();
 
     return defaultExtension.equalsIgnoreCase(extension);
   }
 
-  private boolean isCommentOrSpaces(final @NotNull VirtualFile file, final int line, final @NotNull Project project) {
+  private boolean isStoppable(@NotNull final VirtualFile file, final int line, @NotNull final Project project) {
     final PsiFile psiFile = PsiManager.getInstance(project).findFile(file);
     final Document document = PsiDocumentManager.getInstance(project).getDocument(psiFile);
+    final boolean[] justResult = new boolean[]{false};
 
-    final String text = document.getText(
-      new TextRange(
-        document.getLineStartOffset(line),
-        document.getLineEndOffset(line)
-      )
+    XDebuggerUtil.getInstance().iterateLine(
+      project,
+      document,
+      line,
+      new Processor<PsiElement>() {
+        @Override
+        public boolean process(@NotNull final PsiElement element) {
+          if (isNotStoppable(element) || isNotStoppable(element.getNode().getElementType())) return true;
+
+          justResult[0] = true;
+          return false;
+        }
+      }
     );
 
-    return TheRDebuggerUtils.isCommentOrSpaces(text);
+    return justResult[0];
+  }
+
+  private boolean isNotStoppable(@NotNull final PsiElement element) {
+    return element instanceof PsiWhiteSpace || element instanceof PsiComment;
+  }
+
+  private boolean isNotStoppable(@NotNull final IElementType type) {
+    return type == TheRElementTypes.THE_R_LBRACE || type == TheRElementTypes.THE_R_RBRACE;
   }
 }
