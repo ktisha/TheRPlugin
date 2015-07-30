2f46ec182ec8c2cfb4e2feb7667d2528734a15b5
Semyon Proshev <s-proshev@ya.ru>
2015-07-30 15:01:11 +0300

    Debugger: test append methods in string utils

--- test/com/jetbrains/ther/debugger/TheRDebuggerStringUtilsTest.java
+++ test/com/jetbrains/ther/debugger/TheRDebuggerStringUtilsTest.java
@@ -1,11 +1,77 @@
 package com.jetbrains.ther.debugger;
 
+import com.intellij.openapi.util.TextRange;
+import com.jetbrains.ther.debugger.interpreter.TheRProcessResponse;
+import com.jetbrains.ther.debugger.interpreter.TheRProcessResponseType;
+import com.jetbrains.ther.debugger.mock.IllegalTheROutputReceiver;
+import com.jetbrains.ther.debugger.mock.TheROutputErrorReceiver;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
 import static com.jetbrains.ther.debugger.TheRDebuggerStringUtils.*;
 import static org.junit.Assert.*;
 
 public class TheRDebuggerStringUtilsTest {
+
+  @Test
+  public void emptyErrorAppending() {
+    appendError(
+      new TheRProcessResponse("", TheRProcessResponseType.EMPTY, TextRange.EMPTY_RANGE, ""),
+      new IllegalTheROutputReceiver()
+    );
+  }
+
+  @Test
+  public void emptyOutputAppending() {
+    appendOutput(
+      new TheRProcessResponse("", TheRProcessResponseType.EMPTY, TextRange.EMPTY_RANGE, ""),
+      new IllegalTheROutputReceiver()
+    );
+  }
+
+  @Test
+  public void emptyResultAppending() {
+    appendResult(
+      new TheRProcessResponse("abc", TheRProcessResponseType.RESPONSE, TextRange.EMPTY_RANGE, ""),
+      new IllegalTheROutputReceiver()
+    );
+  }
+
+  @Test
+  public void ordinaryErrorAppending() {
+    final TheROutputErrorReceiver receiver = new TheROutputErrorReceiver("error");
+
+    appendError(
+      new TheRProcessResponse("", TheRProcessResponseType.EMPTY, TextRange.EMPTY_RANGE, "error"),
+      receiver
+    );
+
+    assertEquals(1, receiver.getErrorReceived());
+  }
+
+  @Test
+  public void ordinaryOutputAppending() {
+    final MockTheROutputReceiver receiver = new MockTheROutputReceiver("output");
+
+    appendOutput(
+      new TheRProcessResponse("output", TheRProcessResponseType.RESPONSE, TextRange.allOf("output"), ""),
+      receiver
+    );
+
+    assertEquals(1, receiver.myOutputReceived);
+  }
+
+  @Test
+  public void ordinaryResultAppending() {
+    final MockTheROutputReceiver receiver = new MockTheROutputReceiver("out");
+
+    appendResult(
+      new TheRProcessResponse("output", TheRProcessResponseType.RESPONSE, new TextRange(0, 3), ""),
+      receiver
+    );
+
+    assertEquals(1, receiver.myOutputReceived);
+  }
 
   @Test
   public void commentChecking() {
@@ -81,5 +147,30 @@ public class TheRDebuggerStringUtilsTest {
     final String text = "abc\ndef";
 
     assertEquals(3, findCurrentLineEnd(text, 0));
+  }
+
+  private static class MockTheROutputReceiver implements TheROutputReceiver {
+
+    @NotNull
+    private final String myExpectedOutput;
+
+    private int myOutputReceived;
+
+    public MockTheROutputReceiver(@NotNull final String expectedOutput) {
+      myExpectedOutput = expectedOutput;
+      myOutputReceived = 0;
+    }
+
+    @Override
+    public void receiveOutput(@NotNull final String output) {
+      myOutputReceived++;
+
+      assertEquals(myExpectedOutput, output);
+    }
+
+    @Override
+    public void receiveError(@NotNull final String error) {
+      throw new IllegalStateException("ReceiveError shouldn't be called");
+    }
   }
 }
