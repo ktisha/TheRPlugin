e6d26b7525c74332b3cb8c2363d6d033675ca15e
Semyon Proshev <s-proshev@ya.ru>
2015-09-14 16:42:16 +0300

    Debugger: add working dir and envs to run configuration

--- src/com/jetbrains/ther/run/TheRRunConfigurationForm.java
+++ src/com/jetbrains/ther/run/TheRRunConfigurationForm.java
@@ -1,46 +1,41 @@
 package com.jetbrains.ther.run;
 
-import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.execution.configuration.EnvironmentVariablesComponent;
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.ComponentWithBrowseButton;
 import com.intellij.openapi.ui.TextComponentAccessor;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
-import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.ui.PanelWithAnchor;
 import com.intellij.ui.RawCommandLineEditor;
-import com.intellij.ui.components.JBLabel;
+import com.jetbrains.ther.TheRFileType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 
-public class TheRRunConfigurationForm implements TheRRunConfigurationParams, PanelWithAnchor {
+public class TheRRunConfigurationForm implements TheRRunConfigurationParams {
+
   private JPanel myRootPanel;
   private TextFieldWithBrowseButton myScriptTextField;
   private RawCommandLineEditor myScriptParametersTextField;
-  private JBLabel myScriptParametersLabel;
-  private JComponent anchor;
+  private TextFieldWithBrowseButton myWorkingDirectoryTextField;
+  private EnvironmentVariablesComponent myEnvsComponent;
 
-  public TheRRunConfigurationForm(TheRRunConfiguration configuration) {
+  public TheRRunConfigurationForm(@NotNull final TheRRunConfiguration configuration) {
     final Project project = configuration.getProject();
-    FileChooserDescriptor chooserDescriptor = new FileChooserDescriptor(true, false, false, false, false, false) {
-      @Override
-      public boolean isFileVisible(VirtualFile file, boolean showHiddenFiles) {
-        return file.isDirectory() || file.getExtension() == null || Comparing.equal(file.getExtension(), "r");
-      }
-    };
-    ComponentWithBrowseButton.BrowseFolderActionListener<JTextField> listener =
-      new ComponentWithBrowseButton.BrowseFolderActionListener<JTextField>("Select Script", "", myScriptTextField, project,
-                                                                           chooserDescriptor, TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT);
-    myScriptTextField.addActionListener(listener);
+
+    setupScriptTextField(project);
+    setupScriptParametersTextField();
+    setupWorkingDirectoryTextField(project);
   }
 
+  @NotNull
   public JComponent getPanel() {
     return myRootPanel;
   }
 
+  @NotNull
   @Override
   public String getScriptName() {
     return FileUtil.toSystemIndependentName(myScriptTextField.getText().trim());
@@ -51,24 +46,41 @@ public class TheRRunConfigurationForm implements TheRRunConfigurationParams, Pan
     myScriptTextField.setText(scriptName == null ? "" : FileUtil.toSystemDependentName(scriptName));
   }
 
+  @NotNull
   @Override
   public String getScriptParameters() {
     return myScriptParametersTextField.getText().trim();
   }
 
   @Override
-  public void setScriptParameters(@NotNull String scriptParameters) {
+  public void setScriptParameters(@NotNull final String scriptParameters) {
     myScriptParametersTextField.setText(scriptParameters);
   }
 
-  @Override
-  public JComponent getAnchor() {
-    return anchor;
+  private void setupScriptTextField(@NotNull final Project project) {
+    final ComponentWithBrowseButton.BrowseFolderActionListener<JTextField> listener =
+      new ComponentWithBrowseButton.BrowseFolderActionListener<JTextField>(
+        "Select Script",
+        "",
+        myScriptTextField,
+        project,
+        FileChooserDescriptorFactory.createSingleFileDescriptor(TheRFileType.INSTANCE),
+        TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT
+      );
+
+    myScriptTextField.addActionListener(listener);
   }
 
-  @Override
-  public void setAnchor(JComponent anchor) {
-    this.anchor = anchor;
-    myScriptParametersLabel.setAnchor(anchor);
+  private void setupScriptParametersTextField() {
+    myScriptParametersTextField.setDialogCaption("Script Parameters");
+  }
+
+  private void setupWorkingDirectoryTextField(@NotNull final Project project) {
+    myWorkingDirectoryTextField.addBrowseFolderListener(
+      "Select Working Directory",
+      "",
+      project,
+      FileChooserDescriptorFactory.createSingleFolderDescriptor()
+    );
   }
 }
