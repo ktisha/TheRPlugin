a1e21851b1e3383af1e80fd4c262bdce55d7af96
Semyon Proshev <s-proshev@ya.ru>
2016-01-22 15:45:37 +0300

    [run] NotNull annotations and package-private access in run config editor

--- src/com/jetbrains/ther/run/configuration/TheRRunConfigurationEditor.java
+++ src/com/jetbrains/ther/run/configuration/TheRRunConfigurationEditor.java
@@ -6,20 +6,21 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
-public class TheRRunConfigurationEditor extends SettingsEditor<TheRRunConfiguration> {
+class TheRRunConfigurationEditor extends SettingsEditor<TheRRunConfiguration> {
+
   private TheRRunConfigurationForm myForm;
 
-  public TheRRunConfigurationEditor(final TheRRunConfiguration configuration) {
+  public TheRRunConfigurationEditor(@NotNull final TheRRunConfiguration configuration) {
     myForm = new TheRRunConfigurationForm(configuration);
   }
 
   @Override
-  protected void resetEditorFrom(final TheRRunConfiguration config) {
+  protected void resetEditorFrom(@NotNull final TheRRunConfiguration config) {
     TheRRunConfiguration.copyParams(config, myForm);
   }
 
   @Override
-  protected void applyEditorTo(final TheRRunConfiguration config) throws ConfigurationException {
+  protected void applyEditorTo(@NotNull final TheRRunConfiguration config) throws ConfigurationException {
     TheRRunConfiguration.copyParams(myForm, config);
   }
 
