1cb009795344f4386cd120446b9ac50cb799eb61
Semyon Proshev <s-proshev@ya.ru>
2016-01-27 17:40:16 +0300

    [run] extract command line calculation to special class

---
+++ src/com/jetbrains/ther/run/TheRCommandLineCalculator.java
@@ -0,0 +1,44 @@
+package com.jetbrains.ther.run;
+
+import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.execution.configurations.GeneralCommandLine.ParentEnvironmentType;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.util.execution.ParametersListUtil;
+import com.jetbrains.ther.debugger.executor.TheRProcessUtils;
+import com.jetbrains.ther.run.configuration.TheRRunConfiguration;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// TODO [run][test]
+final class TheRCommandLineCalculator {
+
+  @NotNull
+  public static GeneralCommandLine calculateCommandLine(@NotNull final String interpreterPath,
+                                                        @NotNull final TheRRunConfiguration runConfiguration) {
+    return new GeneralCommandLine(calculateCommand(interpreterPath, runConfiguration))
+      .withWorkDirectory(runConfiguration.getWorkingDirectoryPath())
+      .withEnvironment(runConfiguration.getEnvs())
+      .withParentEnvironmentType(runConfiguration.isPassParentEnvs() ? ParentEnvironmentType.CONSOLE : ParentEnvironmentType.NONE);
+  }
+
+  @NotNull
+  private static List<String> calculateCommand(@NotNull final String interpreterPath,
+                                               @NotNull final TheRRunConfiguration runConfiguration) {
+    final List<String> command = new ArrayList<String>();
+
+    command.add(FileUtil.toSystemDependentName(interpreterPath));
+    command.addAll(TheRProcessUtils.getStartOptions());
+
+    final String scriptArgs = runConfiguration.getScriptArgs();
+
+    if (!StringUtil.isEmptyOrSpaces(scriptArgs)) {
+      command.add("--args");
+      command.addAll(ParametersListUtil.parse(scriptArgs));
+    }
+
+    return command;
+  }
+}
--- src/com/jetbrains/ther/run/TheRCommandLineState.java
+++ src/com/jetbrains/ther/run/TheRCommandLineState.java
@@ -3,37 +3,25 @@ package com.jetbrains.ther.run;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.CommandLineState;
 import com.intellij.execution.configurations.GeneralCommandLine;
-import com.intellij.execution.configurations.GeneralCommandLine.ParentEnvironmentType;
 import com.intellij.execution.executors.DefaultDebugExecutor;
 import com.intellij.execution.process.ProcessHandler;
 import com.intellij.execution.process.ProcessTerminatedListener;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.openapi.options.ConfigurationException;
-import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.util.execution.ParametersListUtil;
 import com.jetbrains.ther.debugger.executor.TheRExecutionResultCalculator;
 import com.jetbrains.ther.debugger.executor.TheRExecutionResultCalculatorImpl;
-import com.jetbrains.ther.debugger.executor.TheRProcessUtils;
 import com.jetbrains.ther.interpreter.TheRInterpreterService;
 import com.jetbrains.ther.run.configuration.TheRRunConfiguration;
 import com.jetbrains.ther.run.configuration.TheRRunConfigurationUtils;
 import com.jetbrains.ther.run.run.TheRRunExecutionResultCalculator;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static java.lang.Boolean.parseBoolean;
 
-// TODO [run][test]
 public class TheRCommandLineState extends CommandLineState {
 
   @NotNull
   private static final String IO_ENV_KEY = "ther.debugger.io";
-
-  @NotNull
-  private final String myInterpreterPath;
 
   @NotNull
   private final TheRRunConfiguration myRunConfiguration;
@@ -41,7 +29,6 @@ public class TheRCommandLineState extends CommandLineState {
   public TheRCommandLineState(@NotNull final ExecutionEnvironment environment, @NotNull final TheRRunConfiguration runConfiguration) {
     super(environment);
 
-    myInterpreterPath = TheRInterpreterService.getInstance().getInterpreterPath();
     myRunConfiguration = runConfiguration;
   }
 
@@ -50,7 +37,13 @@ public class TheRCommandLineState extends CommandLineState {
   protected ProcessHandler startProcess() throws ExecutionException {
     checkRunConfiguration();
 
-    final ProcessHandler processHandler = startProcess(myRunConfiguration, calculateCommandLine());
+    final ProcessHandler processHandler = startProcess(
+      myRunConfiguration,
+      TheRCommandLineCalculator.calculateCommandLine(
+        TheRInterpreterService.getInstance().getInterpreterPath(),
+        myRunConfiguration
+      )
+    );
 
     ProcessTerminatedListener.attach(processHandler, myRunConfiguration.getProject());
 
@@ -68,20 +61,12 @@ public class TheRCommandLineState extends CommandLineState {
 
   @NotNull
   private ProcessHandler startProcess(@NotNull final TheRRunConfiguration runConfiguration,
-                                      @NotNull final GeneralCommandLine generalCommandLine) throws ExecutionException {
+                                      @NotNull final GeneralCommandLine commandLine) throws ExecutionException {
     return new TheRXProcessHandler(
-      generalCommandLine,
+      commandLine,
       createExecutionResultCalculator(),
       parseBoolean(runConfiguration.getEnvs().get(IO_ENV_KEY))
     );
-  }
-
-  @NotNull
-  private GeneralCommandLine calculateCommandLine() {
-    return new GeneralCommandLine(calculateCommand())
-      .withWorkDirectory(myRunConfiguration.getWorkingDirectoryPath())
-      .withEnvironment(myRunConfiguration.getEnvs())
-      .withParentEnvironmentType(myRunConfiguration.isPassParentEnvs() ? ParentEnvironmentType.CONSOLE : ParentEnvironmentType.NONE);
   }
 
   @NotNull
@@ -92,22 +77,5 @@ public class TheRCommandLineState extends CommandLineState {
     else {
       return new TheRRunExecutionResultCalculator();
     }
-  }
-
-  @NotNull
-  private List<String> calculateCommand() {
-    final List<String> command = new ArrayList<String>();
-
-    command.add(FileUtil.toSystemDependentName(myInterpreterPath));
-    command.addAll(TheRProcessUtils.getStartOptions());
-
-    final String scriptArgs = myRunConfiguration.getScriptArgs();
-
-    if (!StringUtil.isEmptyOrSpaces(scriptArgs)) {
-      command.add("--args");
-      command.addAll(ParametersListUtil.parse(scriptArgs));
-    }
-
-    return command;
   }
 }
