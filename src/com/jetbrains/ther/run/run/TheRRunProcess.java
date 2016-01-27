eac6e4269afaf61a2a041e8e7b37d71d6096ab6c
Semyon Proshev <s-proshev@ya.ru>
2016-01-27 14:21:01 +0300

    [run] extract run process from run runner: runner prepares process and then starts it, process handles execution of script

---
+++ src/com/jetbrains/ther/run/run/TheRRunProcess.java
@@ -0,0 +1,120 @@
+package com.jetbrains.ther.run.run;
+
+import com.intellij.execution.ExecutionResult;
+import com.intellij.execution.process.ProcessAdapter;
+import com.intellij.execution.process.ProcessEvent;
+import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.execution.runners.RunContentBuilder;
+import com.intellij.execution.ui.RunContentDescriptor;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import com.jetbrains.ther.debugger.exception.TheRDebuggerException;
+import com.jetbrains.ther.run.TheRXProcessHandler;
+import com.jetbrains.ther.run.graphics.TheRGraphicsUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+
+class TheRRunProcess {
+
+  @NotNull
+  private static final Logger LOGGER = Logger.getInstance(TheRRunProcess.class);
+
+  @NotNull
+  private final Project myProject;
+
+  @NotNull
+  private final ExecutionEnvironment myEnvironment;
+
+  @NotNull
+  private final ExecutionResult myExecutionResult;
+
+  @NotNull
+  private final List<String> myInitCommands;
+
+  @NotNull
+  private final ExecutorService myExecutor;
+
+  public TheRRunProcess(@NotNull final Project project,
+                        @NotNull final ExecutionEnvironment environment,
+                        @NotNull final ExecutionResult executionResult,
+                        @NotNull final List<String> initCommands,
+                        @NotNull final ExecutorService executor) {
+    myProject = project;
+    myEnvironment = environment;
+    myExecutionResult = executionResult;
+    myInitCommands = initCommands;
+    myExecutor = executor;
+  }
+
+  @NotNull
+  public RunContentDescriptor getRunContentDescriptor() {
+    TheRGraphicsUtils.getGraphicsState(myProject).reset();
+
+    final TheRXProcessHandler processHandler = (TheRXProcessHandler)myExecutionResult.getProcessHandler();
+
+    processHandler.addListener(new InitializationProcessListener(processHandler, myInitCommands, myExecutor));
+    processHandler.addProcessListener(new TerminationProcessListener(myProject));
+
+    return new RunContentBuilder(myExecutionResult, myEnvironment).showRunContent(myEnvironment.getContentToReuse());
+  }
+
+  private static class InitializationProcessListener implements TheRXProcessHandler.Listener {
+
+    @NotNull
+    private final TheRXProcessHandler myProcessHandler;
+
+    @NotNull
+    private final List<String> myInitCommands;
+
+    @NotNull
+    private final ExecutorService myExecutor;
+
+    public InitializationProcessListener(@NotNull final TheRXProcessHandler processHandler,
+                                         @NotNull final List<String> initCommands,
+                                         @NotNull final ExecutorService executor) {
+      myProcessHandler = processHandler;
+      myInitCommands = initCommands;
+      myExecutor = executor;
+    }
+
+    @Override
+    public void onInitialized() {
+      myExecutor.submit(
+        new Runnable() {
+          @Override
+          public void run() {
+            try {
+              for (final String initCommand : myInitCommands) {
+                myProcessHandler.execute(initCommand);
+              }
+            }
+            catch (final TheRDebuggerException e) {
+              LOGGER.error(e);
+            }
+            finally {
+              myExecutor.shutdown();
+            }
+          }
+        }
+      );
+    }
+  }
+
+  private static class TerminationProcessListener extends ProcessAdapter {
+
+    @NotNull
+    private final Project myProject;
+
+    public TerminationProcessListener(@NotNull final Project project) {
+      myProject = project;
+    }
+
+    @Override
+    public void processTerminated(@Nullable final ProcessEvent event) {
+      TheRGraphicsUtils.getGraphicsState(myProject).refresh(false);
+    }
+  }
+}
--- src/com/jetbrains/ther/run/run/TheRRunRunner.java
+++ src/com/jetbrains/ther/run/run/TheRRunRunner.java
@@ -5,32 +5,22 @@ import com.intellij.execution.ExecutionResult;
 import com.intellij.execution.configurations.RunProfile;
 import com.intellij.execution.configurations.RunProfileState;
 import com.intellij.execution.executors.DefaultRunExecutor;
-import com.intellij.execution.process.ProcessAdapter;
-import com.intellij.execution.process.ProcessEvent;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.execution.runners.GenericProgramRunner;
-import com.intellij.execution.runners.RunContentBuilder;
 import com.intellij.execution.ui.RunContentDescriptor;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.util.ConcurrencyUtil;
 import com.jetbrains.ther.debugger.data.TheRDebugConstants;
-import com.jetbrains.ther.debugger.exception.TheRDebuggerException;
-import com.jetbrains.ther.run.TheRXProcessHandler;
+import com.jetbrains.ther.run.TheRCommandLineState;
 import com.jetbrains.ther.run.configuration.TheRRunConfiguration;
 import com.jetbrains.ther.run.graphics.TheRGraphicsUtils;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ThreadPoolExecutor;
 
 public class TheRRunRunner extends GenericProgramRunner {
-
-  @NotNull
-  private static final Logger LOGGER = Logger.getInstance(TheRRunRunner.class);
 
   @NotNull
   private static final String RUNNER_ID = "TheRRunRunner";
@@ -54,19 +44,24 @@ public class TheRRunRunner extends GenericProgramRunner {
     throws ExecutionException {
     FileDocumentManager.getInstance().saveAllDocuments();
 
-    final ExecutionResult executionResult = state.execute(environment.getExecutor(), this);
-    assert executionResult != null;
-
     final Project project = environment.getProject();
-    final TheRXProcessHandler processHandler = (TheRXProcessHandler)executionResult.getProcessHandler();
     final TheRRunConfiguration runConfiguration = (TheRRunConfiguration)environment.getRunProfile();
 
-    TheRGraphicsUtils.getGraphicsState(project).reset();
+    return new TheRRunProcess(
+      project,
+      environment,
+      getExecutionResult(state, environment),
+      calculateInitCommands(runConfiguration),
+      ConcurrencyUtil.newSingleThreadExecutor(EXECUTOR_NAME)
+    ).getRunContentDescriptor();
+  }
 
-    processHandler.addListener(new InitializationProcessListener(processHandler, calculateInitCommands(runConfiguration)));
-    processHandler.addProcessListener(new TerminationProcessListener(project));
+  @NotNull
+  private ExecutionResult getExecutionResult(@NotNull final RunProfileState state, @NotNull final ExecutionEnvironment environment)
+    throws ExecutionException {
+    final TheRCommandLineState commandLineState = (TheRCommandLineState)state;
 
-    return new RunContentBuilder(executionResult, environment).showRunContent(environment.getContentToReuse());
+    return commandLineState.execute(environment.getExecutor(), this);
   }
 
   @NotNull
@@ -80,58 +75,5 @@ public class TheRRunRunner extends GenericProgramRunner {
     result.add(TheRDebugConstants.QUIT_COMMAND);
 
     return result;
-  }
-
-  private static class InitializationProcessListener implements TheRXProcessHandler.Listener {
-
-    @NotNull
-    private final TheRXProcessHandler myProcessHandler;
-
-    @NotNull
-    private final List<String> myInitCommands;
-
-    public InitializationProcessListener(@NotNull final TheRXProcessHandler processHandler, @NotNull final List<String> initCommands) {
-      myProcessHandler = processHandler;
-      myInitCommands = initCommands;
-    }
-
-    @Override
-    public void onInitialized() {
-      final ThreadPoolExecutor executor = ConcurrencyUtil.newSingleThreadExecutor(EXECUTOR_NAME);
-
-      executor.submit(
-        new Runnable() {
-          @Override
-          public void run() {
-            try {
-              for (final String initCommand : myInitCommands) {
-                myProcessHandler.execute(initCommand);
-              }
-            }
-            catch (final TheRDebuggerException e) {
-              LOGGER.error(e);
-            }
-            finally {
-              executor.shutdown();
-            }
-          }
-        }
-      );
-    }
-  }
-
-  private static class TerminationProcessListener extends ProcessAdapter {
-
-    @NotNull
-    private final Project myProject;
-
-    public TerminationProcessListener(@NotNull final Project project) {
-      myProject = project;
-    }
-
-    @Override
-    public void processTerminated(@Nullable final ProcessEvent event) {
-      TheRGraphicsUtils.getGraphicsState(myProject).refresh(false);
-    }
   }
 }
